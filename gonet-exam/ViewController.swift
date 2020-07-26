081789ffd696f73334f76bdacb0d00d3e50ab363
Carlos Ponce <carlosponce156@gmail.com>
2020-07-26 13:49:25 -0500

    Initial Commit

---
+++ gonet-exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gonet-exam
+//
+//  Created by Carlos Ponce on 26/07/20.
+//  Copyright © 2020 Carlos Ponce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
